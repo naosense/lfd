71c4fee89008d715512bbb8b14e7abc19873860c
pingao <pingao777@gmail.com>
2017-08-25 18:21:06 +0800

    使用R编写拟合函数

---
+++ homework1/plot_fit.R
@@ -0,0 +1,26 @@
+plot_fit <- function(f, x, y) {
+    n <- ncol(x)
+    if (n == 1) {
+        xlim <- lim(x)
+        plot(xlim, f(xlim), "l")
+        points(x, y)
+    } else if (n == 2) {
+        x1lim <- lim(x[, 1, drop = F])
+        x2lim <- lim(x[, 2, drop = F])
+        z <- outer(x1lim, x2lim, f);
+        contour(x1lim, x2lim, z, levels = c(0), drawlabels = F);
+        yk <- unique(y);
+        for (i in seq_along(yk)) {
+            ind <- y == yk[i]
+            points(x[ind, 1, drop = F], x[ind, 2, drop = F], pch = i);
+        }
+    }
+
+}
+
+lim <- function(x) {
+    xmin <- min(x)
+    xmax <- max(x)
+    xinterval <- xmax - xmin
+    xlim <- seq(xmin - .1 * xinterval, xmax + .1 * xinterval, length.out = 100)
+}
