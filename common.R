d58d0ed9ddacb0d71c00ac568f295acd32d9492e
wocanmei <pingao777@gmail.com>
2017-08-27 14:50:42 +0800

    设置颜色和点样式

--- common.R
+++ common.R
@@ -1,3 +1,4 @@
+colors <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999")
 pause <- function() {
   readline(prompt="Press [enter] to continue")
 }
@@ -14,17 +15,17 @@ plot_fit <- function(f, x, y) {
   n <- ncol(x)
   if (n == 1) {
     xlim <- lim(x)
-    plot(xlim, f(xlim), "l")
-    points(x, y)
+    plot(xlim, f(xlim), "l", lwd = 2)
+    points(x, y, pch = 20, col = colors[1])
   } else if (n == 2) {
     x1lim <- lim(x[, 1, drop = F])
     x2lim <- lim(x[, 2, drop = F])
     z <- outer(x1lim, x2lim, f);
-    contour(x1lim, x2lim, z, levels = c(0), drawlabels = F);
+    contour(x1lim, x2lim, z, levels = c(0), drawlabels = F, lwd = 2);
     yk <- unique(y);
     for (i in seq_along(yk)) {
       ind <- y == yk[i]
-      points(v(x, 1)[ind], v(x, 2)[ind], pch = i, col = i);
+      points(v(x, 1)[ind], v(x, 2)[ind], pch = 20, col = colors[i]);
     }
   }
 
