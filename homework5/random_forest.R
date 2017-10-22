1ec274464ffdf527479eb1ab132394fec5a33c4f
wocanmei <pingao777@gmail.com>
2017-10-22 22:02:39 +0800

    计算离群点

--- homework5/random_forest.R
+++ homework5/random_forest.R
@@ -1,43 +1,35 @@
 source("common.R")
-random_forest <- function(data, ts = 10, feature_count = as.integer(sqrt(ncol(data)))) {
-  set.seed(1)
+random_forest <- function(data, ts = 10L, feature_count = as.integer(sqrt(ncol(data)))) {
   marjority <- function(v) {
-    name <- names(which.max(table(v)))
+    ifelse(length(v) <= 0L, "NULL", names(which.max(table(v))))
   }
   cart_decision_tree <- function(data, type = "class") {
-    impurify <- function(data, sp, j) {
-      left <- NULL
-      right <- NULL
-      if (is.factor(sp) || is.character(sp)) {
-        left <- h(data, data[, j] == sp)
-        right <- h(data, data[, j] != sp)
+    impufity <- function(data, sp, j) {
+      if (type_array[j]) {
+        left_cond <- data[, j] == sp
       } else {
-        left <- h(data, data[, j] < sp)
-        right <- h(data, data[, j] >= sp)
+        left_cond <- data[, j] < sp
       }
+      left <- h(data, left_cond)
+      right <- h(data, !left_cond)
       N <- nrow(data)
-      impurify <- sum(nrow(left) / N * impurify_one_group(left),
-                      nrow(right) / N * impurify_one_group(right))
-      list(impurify = impurify, sp = sp, left = left, right = right)
+      impufity <- sum(nrow(left) / N * impurity_one_group(left),
+                      nrow(right) / N * impurity_one_group(right))
+      list(impufity = impufity, ind = j, sp = sp, left = left, right = right)
     }
 
-    impurify_one_group <- function(data) {
-      if (nrow(data) == 0) {
-        return(0)
+    impurity_one_group <- function(data) {
+      if (nrow(data) == 0L) {
+        return(0L)
       }
       y <- data[, ncol(data)]
       N <- nrow(data)
       p <- table(y) / N
       sum(p * (1 - p))
-      # clazz <- unique(y)
-      # sum(vapply(clazz, function(c) {
-      #   p <- sum(y == c) / N
-      #   p * (1 - p)
-      # }, FUN.VALUE = double(1)))
     }
 
     split_branch <- function(data) {
-      if (nrow(data) == 0) {
+      if (nrow(data) == 0L) {
         return()
       }
       rows <- nrow(data)
@@ -56,107 +48,120 @@ random_forest <- function(data, ts = 10, feature_count = as.integer(sqrt(ncol(da
         # same x stop
         return(marjority(y))
       } else {
-        min_impurify <- 10L
-        min_sp <- 0L
-        ind <- 0L
-        left <- NULL
-        right <- NULL
-        sps <- NULL
+        min_res <- list(impufity = 10L, ind = 0L, sp = 0L, left = NULL, right = NULL)
         uniq_cols <- unique(feature_selected)
         for (j in uniq_cols) {
           sps <- unique(data[, j])
           for (sp in sps) {
-            res <- impurify(data, sp, j)
-            if (min_impurify > res[["impurify"]]) {
-              min_impurify <- res[["impurify"]]
-              ind <- j
-              min_sp <- sp
-              left <- res[["left"]]
-              right <- res[["right"]]
+            res <- impufity(data, sp, j)
+            if (min_res[["impufity"]] > res[["impufity"]]) {
+              min_res <- res
             }
           }
         }
-        tree <- list(name = names(data)[ind],
-                     ind = ind,
-                     sp = min_sp,
-                     left = split_branch(left),
-                     right = split_branch(right))
+        tree <- list(name = names[min_res[["ind"]]],
+                     ind = min_res[["ind"]],
+                     sp = min_res[["sp"]],
+                     left = split_branch(min_res[["left"]]),
+                     right = split_branch(min_res[["right"]]))
       }
       tree
     }
+    names <- colnames(data)
     split_branch(data)
   }
   predict_forest <- function(trees, data) {
-    vote <- function(trees, x) {
-      vapply(trees, function(tree) predict_tree(tree, x), FUN.VALUE = character(1))
-    }
-    if (length(trees) <= 0) {
-      return("NULL")
+    if (length(trees) <= 0L) {
+      return("NOTREE")
     }
     N <- nrow(data)
-    vapply(1:N, function(i) marjority(vote(trees, h(data, i))), FUN.VALUE = character(1))
-  }
-
-  predict_tree_in <- function(tree, data) {
-    N <- nrow(data)
-    vapply(1:N, function(i) predict_tree(tree, h(data, i)), FUN.VALUE = character(1))
-  }
-
-  predict_tree <- function(tree, x) {
-    if (is.atomic(tree)) {
-      return(tree)
+    if (is.null(N) || N == 1L) {
+      marjority(vapply(trees, function(t) predict_tree(t, data), y_type(1)))
     } else {
-      if (is.factor(tree[["sp"]]) || is.character(tree[["sp"]])) {
-        if (x[1, tree[["ind"]]] == tree[["sp"]]) {
-          return(predict_tree(tree[["left"]], x))
-        } else {
-          return(predict_tree(tree[["right"]], x))
-        }
+      predict_matrix <- vapply(trees, function(t) predict_tree(t, data), y_type(N))
+      vapply(1:N,function(r) marjority(predict_matrix[r,]), y_type(1))
+    }
+  }
+
+  predict_tree <- function(tree, data) {
+    predict_tree_inner <- function(tree, x) {
+      if (is.atomic(tree)) {
+        return(tree)
       } else {
-        if (x[1, tree[["ind"]]] < tree[["sp"]]) {
-          return(predict_tree(tree[["left"]], x))
+        if (type_array[tree[["ind"]]]) {
+          if (x[tree[["ind"]]] == tree[["sp"]]) {
+            return(predict_tree_inner(tree[["left"]], x))
+          } else {
+            return(predict_tree_inner(tree[["right"]], x))
+          }
         } else {
-          return(predict_tree(tree[["right"]], x))
+          if (x[tree[["ind"]]] < tree[["sp"]]) {
+            return(predict_tree_inner(tree[["left"]], x))
+          } else {
+            return(predict_tree_inner(tree[["right"]], x))
+          }
         }
       }
+    }
+    if (is.null(nrow(data)) || nrow(data) == 1L) {
+      predict_tree_inner(tree, data)
+    } else {
+      vapply(1:nrow(data), function(r) predict_tree_inner(tree, h(data, r)), y_type(1))
     }
   }
 
   N <- nrow(data)
   M <- ncol(data)
+  origin_name <- colnames(data)
+  type_array <- vapply(1:M, function(c) is.factor(data[1, c]) || is.character(data[1, c]), logical(1))
+  y_type <- ifelse(type_array[M], character, numeric)
+  data <- data.matrix(data)
   test <- sample(1:N, as.integer(0.1 * N))
   train <- setdiff(1:N, test)
   y <- data[, M]
   trees <- rep(list(NULL), ts)
-  # oob_errors <- rep(0, ts)
-  oob_matrix <- matrix(rep(0, N * ts), nrow = N)
-  importance <- rep(0, M - 1)
-  proximities <- matrix(rep(0L, N * N), nrow = N)
-  yt <- rep(0, ts)
+  oob_matrix <- matrix(F, N, ts)
+  importance <- rep(0L, M - 1)
+  proximities <- matrix(0L, N, N)
+  message("Trainning forest...")
   invisible(capture.output(pb <- txtProgressBar(0, 100, width = 80, style = 3)))
   for (i in 1:ts) {
     row_selected <- sample(train, length(train), replace = T)
     trees[[i]] <- cart_decision_tree(data[row_selected, ])
-    oob_matrix[row_selected, i] <- 1
-    setTxtProgressBar(pb, i/ts * 100)
+    oob_matrix[row_selected, i] <- T
+    setTxtProgressBar(pb, i/ts * 100L)
   }
 
-  oob_error <- sum(vapply(train, function(i) {
-    predict_forest(trees[oob_matrix[i, ] == 0], h(data, i)) != y[i]
-  }, FUN.VALUE = logical(1)))/length(train)
+  message("\nComputing oob error...")
 
+  predict_matrix <- vapply(trees, function(t) predict_tree(t, data), y_type(N))
+  oob_error <- sum(vapply(train, function(r) {
+    marjority(predict_matrix[r,][!oob_matrix[r, ]])
+  }, y_type(1)) == y[train]) / length(train)
+
+  message("Computing feature importance...")
   importance <- vapply(1:(M - 1), function(j) {
     data[train, j] <- sample(data[train, j], length(train))
     oob_error_perm <- sum(vapply(train, function(i) {
-      predict_forest(trees[oob_matrix[i, ] == 0], h(data, i)) != y[i]
-    }, FUN.VALUE = double(1)))/length(train)
+      predict_forest(trees[!oob_matrix[i, ]], h(data, i))
+    }, FUN.VALUE = y_type(1)) == y[train]) / length(train)
     abs(oob_error_perm - oob_error)
   }, FUN.VALUE = double(1))
 
-  predict_matrix <- vapply(trees, function(t) predict_tree_in(t, data), FUN.VALUE = character(N))
+  message("Computing proximities...")
   proximities <- vapply(1:N, function(i, m) colSums(predict_matrix[i,] == m), FUN.VALUE = double(N), m = t(predict_matrix))
-  test_error <- sum(predict_forest(trees, data[test, ]) != y[test])/length(test)
-  names(importance) <- names(data)[1:(M - 1)]
+  outliers <- N / colSums(proximities ^ 2)
+  for (i in unique(y)) {
+    index <- which(y == i)
+    sd <- sd(outliers[index])
+    if (is.na(sd) || sd == 0L) {
+      next()
+    }
+    outliers[index] <- (outliers[index] - mean(outliers[index])) / sd
+  }
+  test_error <- sum(predict_forest(trees, data[test, ]) != y[test]) / length(test)
+  names(importance) <- origin_name[1:(M - 1)]
   importance <- as.data.frame(importance[order(-importance)], optional = T)
-  return(list(trees = trees, oob_error = oob_error, test_error = test_error, importance = importance, proximities = proximities, data=data))
+  message("Done.")
+  return(list(trees = trees, oob_error = oob_error, test_error = test_error, importance = importance, proximities = proximities, data=data, outliers=outliers))
 }
